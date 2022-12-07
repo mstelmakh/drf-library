2fbf27291b653f6a931abfee5050084f15d4c941
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2022-12-07 14:30:50 +0100

    Update email env variable & add readme

--- src/app/config/settings.py
+++ src/app/config/settings.py
@@ -186,5 +186,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'drf.library.management@gmail.com'
-EMAIL_HOST_PASSWORD = 'weimtyehkecwdjgn'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'drf.library.example@gmail.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'vjcfasedmlnioitq')
